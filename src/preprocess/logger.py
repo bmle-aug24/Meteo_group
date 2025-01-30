166560c9ab32a607ed040a296593f638eb279908
Leila Belmir <leilabelmirr@gmail.com>
2025-01-30 20:04:46 +0100

    Ajout des fichiers logger.py

---
+++ src/preprocess/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+
+# Création du dossier logs s'il n'existe pas
+log_dir = "logs"
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+# Configuration du logger
+logging.basicConfig(
+    filename=os.path.join(log_dir, "service.log"),  # Fichier de logs
+    level=logging.INFO,  # Niveau des logs
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger(__name__)
+
+# Exemple d'utilisation
+logger.info("Logger configuré avec succès !")
+
