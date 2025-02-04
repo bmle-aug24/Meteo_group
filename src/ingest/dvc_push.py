085d19724e47ba58831d837073656d5876189f22
smvelascoc <velascosergio15@gmail.com>
2025-02-04 17:14:37 +0100

    Update_DVC 2025-02-04 17:14:32

--- src/ingest/dvc_push.py
+++ src/ingest/dvc_push.py
@@ -2,9 +2,6 @@ import subprocess as sp
 from datetime import datetime
 
 def main():
-    #repo_path = "/opt/airflow/repo"
-    #sp.run(["mkdir", "-p", repo_path], check=True)
-    #sp.run(["cd", "/opt/airflow/repo"])
     files = ["data/raw"]
     message_commit  = f"Update_DVC {datetime.today().strftime('%Y-%m-%d %H:%M:%S')}"
     for file in files:
@@ -13,6 +10,7 @@ def main():
     sp.run(["dvc", "push"], check=True)
     sp.run(["git", "commit", "-m", message_commit], check=True)
     sp.run(["git", "push"], check=True)
+    print("Data pushed for versioning")
 
 if __name__ == "__main__":
     main()
