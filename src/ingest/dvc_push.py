6198e24c2d0b3e64b4d9459c5adb3a45e72fd3c6
smvelascoc <velascosergio15@gmail.com>
2025-02-04 17:13:51 +0100

    Update_DVC 2025-02-04 17:13:45

--- src/ingest/dvc_push.py
+++ src/ingest/dvc_push.py
@@ -6,11 +6,12 @@ def main():
     #sp.run(["mkdir", "-p", repo_path], check=True)
     #sp.run(["cd", "/opt/airflow/repo"])
     files = ["data/raw"]
+    message_commit  = f"Update_DVC {datetime.today().strftime('%Y-%m-%d %H:%M:%S')}"
     for file in files:
         sp.run(["dvc", "add", file], check=True)
         sp.run(["git", "add", file+".dvc"], check=True)
     sp.run(["dvc", "push"], check=True)
-    sp.run(["git", "commit", "-m", "Update_DVC", datetime.today().strftime(datetime.today().strftime('%Y-%m-%d %H:%M:%S'))], check=True)
+    sp.run(["git", "commit", "-m", message_commit], check=True)
     sp.run(["git", "push"], check=True)
 
 if __name__ == "__main__":
